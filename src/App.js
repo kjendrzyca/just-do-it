d2fa4154088b6d6dc274e76e153c622c52521057
kjendrzyca <kjendrzyca@gmail.com>
2017-05-14 21:48:13 +0200

    added super simple input with list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+let id = 0
+const getId = () => id++
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      todos: [],
+      currentTodoText: ''
+    }
+  }
+
+  changeCurrentTodoText = (event) => {
+    this.setState({currentTodoText: event.target.value})
+  }
+
+  addTodo = () => {
+    this.setState(state => ({
+      todos: state.todos.concat({
+        id: getId(),
+        title: state.currentTodoText
+      }),
+      currentTodoText: ''
+    }))
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,12 +35,21 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          {this.state.todos.map(todo => <div key={todo.id}>{todo.title}</div>)}
+
+          <input
+            type="text"
+            onChange={this.changeCurrentTodoText}
+            value={this.state.currentTodoText}
+          />
+          <button onClick={this.addTodo}>
+            add
+          </button>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
