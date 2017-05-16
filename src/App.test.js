8ceb5d1a6ff7450a78d23743c70d0a2092825ced
kjendrzyca <kjendrzyca@gmail.com>
2017-05-16 21:26:30 +0200

    added integration test for adding and displaying todo

--- src/App.test.js
+++ src/App.test.js
@@ -26,3 +26,46 @@ it('should render AddTodoForm and TodoList', () => {
   expect(component.find('AddTodoForm').length).toBe(1)
   expect(component.find('TodoList').length).toBe(1)
 })
+
+it('should display AddTodoForm and TodoList', () => {
+  // given
+  const addTodoStub = jest.fn()
+
+  // when
+  const component = mount(
+    <App
+      addTodo={addTodoStub}
+    />
+  )
+
+  // then
+  expect(component.find('AddTodoForm').length).toBe(1)
+  expect(component.find('TodoList').length).toBe(1)
+})
+
+it('should display todos added on form', () => {
+  // given
+  const addTodoStub = jest.fn()
+  const component = mount(
+    <App
+      addTodo={addTodoStub}
+    />
+  )
+
+  expect(component.find('Card').length).toBe(0)
+
+  // when
+  const todoTitle = component.find("#todo-title")
+  todoTitle.node.value = 'homework'
+
+  const todoDescription = component.find("#todo-description")
+  todoDescription.node.value = 'math and computer science'
+
+  todoTitle.simulate('change')
+  todoDescription.simulate('change')
+
+  simulateTap(component.find('#add-button').node)
+
+  // then
+  expect(component.find('Card').length).toBe(1)
+})
