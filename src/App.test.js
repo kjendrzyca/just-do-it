161f9380249a7d280e8966edee2718e986a7ee4f
kjendrzyca <kjendrzyca@gmail.com>
2017-05-16 20:11:14 +0200

    Add test for App component to check if it renders AddTodoForm and TodoList

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppComponent from './App';
-import {withMaterialTheme} from './utils'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { mount } from 'enzyme'
+import AppComponent from './App'
+import {withMaterialTheme, simulateTap} from './utils'
 
 const App = withMaterialTheme(AppComponent)
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
+
+it('should render AddTodoForm and TodoList', () => {
+  // given
+  const addTodoStub = jest.fn()
+
+  // when
+  const component = mount(
+    <App
+      addTodo={addTodoStub}
+    />
+  )
+
+  // then
+  expect(component.find('AddTodoForm').length).toBe(1)
+  expect(component.find('TodoList').length).toBe(1)
+})
