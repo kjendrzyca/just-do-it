4f92d34b8820d76dcd7dbf1736c2666367cf9e95
kjendrzyca <kjendrzyca@gmail.com>
2017-05-17 08:45:57 +0200

    added integration test for removing todo

--- src/App.test.js
+++ src/App.test.js
@@ -6,25 +6,9 @@ import {withMaterialTheme, simulateTap} from './utils'
 
 const App = withMaterialTheme(AppComponent)
 
-it('renders without crashing', () => {
+it('should render without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
-})
-
-it('should render AddTodoForm and TodoList', () => {
-  // given
-  const addTodoStub = jest.fn()
-
-  // when
-  const component = mount(
-    <App
-      addTodo={addTodoStub}
-    />
-  )
-
-  // then
-  expect(component.find('AddTodoForm').length).toBe(1)
-  expect(component.find('TodoList').length).toBe(1)
 })
 
 it('should display AddTodoForm and TodoList', () => {
@@ -43,7 +27,7 @@ it('should display AddTodoForm and TodoList', () => {
   expect(component.find('TodoList').length).toBe(1)
 })
 
-it('should display todos added on form', () => {
+it('should display todos added on the form', () => {
   // given
   const addTodoStub = jest.fn()
   const component = mount(
@@ -68,4 +52,40 @@ it('should display todos added on form', () => {
 
   // then
   expect(component.find('Card').length).toBe(1)
+})
+
+it('should remove todos', () => {
+  // given
+  const addTodoStub = jest.fn()
+  const component = mount(
+    <App
+      addTodo={addTodoStub}
+    />
+  )
+
+  expect(component.find('Card').length).toBe(0)
+
+  const todoTitle = component.find("#todo-title")
+  todoTitle.node.value = 'homework'
+
+  const todoDescription = component.find("#todo-description")
+  todoDescription.node.value = 'math and computer science'
+
+  todoTitle.simulate('change')
+  todoDescription.simulate('change')
+
+  simulateTap(component.find('#add-button').node)
+
+  expect(component.find('Card').length).toBe(1)
+
+  // when
+  const removeIcon = component
+    .find('Card').first()
+    .find('TodoRemoveButton').first()
+    .find('svg')
+
+  removeIcon.simulate('click')
+
+  // then
+  expect(component.find('Card').length).toBe(0)
 })
